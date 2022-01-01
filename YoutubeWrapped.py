9030d9e227409f76031709d23cd6d8d2c3d13512
joeuong <38801460+joeuong@users.noreply.github.com>
2022-01-01 17:35:55 -0500

    updated YT wrapped CSV converter

--- YoutubeWrapped.py
+++ YoutubeWrapped.py
@@ -1,33 +1,33 @@
-import json
-import os
-import csv
+def YtMusicCsvConvert():
+    import json
+    import os
+    import csv
 
-use_dir = os.getcwd()
-path = os.path.join(use_dir, 'watch-history.json')
-tar_path = os.path.join(use_dir, '2021YTWrapped.csv')
+    use_dir = os.getcwd()
+    path = os.path.join(use_dir, 'watch-history.json')               #default json file from Google
+    tar_path = os.path.join(use_dir, '2021YTWrapped_1.csv')   #name file whatever you would like
 
-print(tar_path)
+    with open(path, 'r') as df:                                                        #Load data to python 
+        obj = json.loads(df.read())
 
-with open(path, 'r') as df:
-    obj = json.loads(df.read())
+    with open(tar_path,'w', newline ='') as outfile:
+        music = csv.writer(outfile, delimiter='^')                          #lots of commas in artist names, so I used a character that I didn't see ^
+        music.writerow(['songtitle','date','artist','month','hour'])
+        for i in range(len(obj)):
+            if obj[i]['header'] == 'YouTube Music' and obj[i]['time'][0:4] == '2021':        
+                a = obj[i]['title'][8:]                                                      #cleaning titles and artist names
+                a = a.replace(',',' ')
+                b = 'no artist'
+                c = obj[i]['time'][0:10]
+                d = obj[i]['time'][5:7]
+                e = obj[i]['time'][11:13]
+                try: 
+                    b = obj[i]['subtitles'][0]['name']
+                    b = b.replace(' - Topic','')
+                    b = b.replace(',',' ')
+                except KeyError:
+                    continue
+                music.writerow([a,b,c,d,e])                                         #write to CSV
 
-print(obj[0])
-##
-##with open(tar_path,'w', newline ='') as outfile:
-##    music = csv.writer(outfile, delimiter='^')
-##    music.writerow(['songtitle','date','artist','month','hour'])
-##    for i in range(len(obj)):
-##        if obj[i]['header'] == 'YouTube Music' and obj[i]['time'][0:4] == '2021':        
-##            a = obj[i]['title'][8:]
-##            a = a.replace(',',' ')
-##            b = obj[i]['time'][0:10]
-##            c = 'no artist'
-##            d = obj[i]['time'][5:7]
-##            e = obj[i]['time'][11:13]
-##            try: 
-##                c = obj[i]['subtitles'][0]['name']
-##                c = c.replace(' - Topic','')
-##                c = c.replace(',',' ')
-##            except KeyError:
-##                continue
-##            music.writerow([a,b,c,d,e])
+if __name__ == '__main__':
+    YtMusicCsvConvert()
