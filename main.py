d544afcc5e4f0c0ff1937e1533072e338e29c66e
joeuong <38801460+joeuong@users.noreply.github.com>
2022-01-22 14:49:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
 from song_play_details import SongHistory
 
-
 SongHistory()
-
-# Fixing in an later update:
-    # You will need to update the CSV before uncommenting the next function call
-    # Do a Text to Column for the (^) character then remove the results you don't want.
-    # I had 6000 or so rows in my 2021 file, but only tested the iTunes API with up to 100 calls. 
-
-# itunesApiFromInfo()
