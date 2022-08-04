58138c7a8456e4b3b49afbb4fde8c9d73563d121
joeuong <38801460+joeuong@users.noreply.github.com>
2022-08-04 18:04:52 -0400

    Updated with print options
    added Terminal Print Option
    added CSV Print Option

--- YoutubeWrapped.py
+++ YoutubeWrapped.py
@@ -1,39 +1,89 @@
+# Hi Everyone! :) 
+
 def YtMusicCsvConvert():
+    from calendar import month
     import json
     import os
+    from collections import Counter
     import csv
 
     use_dir = os.getcwd()
-    #default json file from Google
-    path = os.path.join(use_dir, 'watch-history.json')     
-    #name file whatever you would like
-    tar_path = os.path.join(use_dir, '2021YTWrapped_1.csv')         
 
-    #Load data to python 
+# Update with your filepath:
+    path = os.path.join(use_dir, 'watch-history.json') 
+
+#Load data to python
     with open(path, 'r') as df:                                                        
         obj = json.loads(df.read())
 
-    with open(tar_path,'w', newline ='') as outfile:
-        #lots of commas in artist names, so I used a character that I didn't see ^
-        music = csv.writer(outfile, delimiter='^')                          
-        music.writerow(['songtitle','date','artist','month','hour'])
-        for i in range(len(obj)):
-            # Only running if Year is 2021
-            # Cleaning titles and artist names
-            if obj[i]['header'] == 'YouTube Music' and obj[i]['time'][0:4] == '2021':        
-                a = obj[i]['title'][8:]                                                     
-                a = a.replace(',',' ')
-                b = 'no artist'
-                c = obj[i]['time'][0:10]
-                d = obj[i]['time'][5:7]
-                e = obj[i]['time'][11:13]
-                try: 
-                    b = obj[i]['subtitles'][0]['name']
-                    b = b.replace(' - Topic','')
-                    b = b.replace(',',' ')
-                except KeyError:
-                    continue
-                music.writerow([a,b,c,d,e])                                         
+#lists for info
+    songs = []
+    artists = []
+    tracks = []
+    dates = []
+    months = []
+    # hours = []
+
+# cleaning titles and artist names
+    for i in range(len(obj)):
+# Update the ['time']'s [0:4] if you want to limit to a specific month 
+# Ex: obj[i]['time'][0:4] >= '2022-08' is anything starting Aug 1, 2022
+        if obj[i]['header'] == 'YouTube Music' and obj[i]['time'][0:4] == '2022':
+            year = obj[i]['time'][0:4]
+            a = obj[i]['title'][8:]
+            a = a.replace(',',' ')
+            b = 'no artist'
+            c = obj[i]['time'][0:10]
+            d = obj[i]['time'][5:7]
+            # e = obj[i]['time'][11:13]
+            try: 
+                b = obj[i]['subtitles'][0]['name']
+                b = b.replace(' - Topic','')
+                b = b.replace(',',' ')
+            except KeyError:
+                continue
+            songs.append(a)
+            artists.append(b)
+            to_track = a + ' // ' + b
+            tracks.append(to_track)
+            dates.append(c)
+            months.append(d)
+            # hours.append(e)
+
+    artistcount = Counter(artists).most_common(20)
+    trackcount = Counter(tracks).most_common(20)
+    monthcount = Counter(months).most_common(12)
+    datescount = Counter(dates).most_common(10)
+    
+# Option to print to Terminal
+    print(f'\nTop Artists of {year}: ')
+    for i in range(len(artistcount)):
+        print(*artistcount[i], sep=', ')
+
+    print(f'\nTop Tracks of {year}: ')
+    for i in range(len(trackcount)):
+        print(*trackcount[i], sep=', ')
+
+    print(f'\nTop Months: ')
+    for i in range(len(monthcount)):
+        print(*monthcount[i], sep=', ')
+
+    print(f'\nTop Dates: ')
+    for i in range(len(datescount)):
+        print(*datescount[i], sep=', ')
+
+# Option to print to CSV
+    # tar_path = os.path.join(use_dir, 'outfile.csv')
+    # with open(tar_path,'w', newline ='') as outfile:
+    #     music = csv.writer(outfile, delimiter=',')
+    #     for i in range(len(trackcount)):
+    #         music.writerow(trackcount[i])
+    
+# Option to see Total Count
+    # a=0
+    # for i in range(len(monthcount)):
+    #    a += int(monthcount[i][1])
+    # print('\n','Total Count: ',a)
 
 if __name__ == '__main__':
     YtMusicCsvConvert()
