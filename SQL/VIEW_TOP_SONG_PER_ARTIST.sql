75fdd5baa2d619457c2833c659ba6fd380e74cc5
Joe Uong <joeuong@gmail.com>
2024-08-08 20:55:19 -0400

    added todolist, new View definition

--- SQL/VIEW_TOP_SONG_PER_ARTIST.sql
+++ SQL/VIEW_TOP_SONG_PER_ARTIST.sql
@@ -2,8 +2,8 @@
 WITH RANKEDSONGS AS (
     -- Select the artist, title, count of listens, and assign a row number within each artist's group
     SELECT
-        MI.G_ARTIST AS ARTIST,           -- Alias for artist column
-        MI.G_TITLE AS TITLE,             -- Alias for title column
+        UPPER(TRIM(MI.G_ARTIST)) AS ARTIST,           -- Alias for artist column
+        UPPER(MI.G_TITLE) AS TITLE,             -- Alias for title column
         COUNT(LH.SONG_URL) AS NUM,       -- Count of listens for each song
         ROW_NUMBER() OVER(PARTITION BY MI.G_ARTIST ORDER BY COUNT(LH.SONG_URL) DESC) AS RN
     FROM
@@ -13,18 +13,32 @@ WITH RANKEDSONGS AS (
     GROUP BY
         MI.G_ARTIST,                     -- Group by artist
         MI.G_TITLE                       -- Group by title
+),
+ARTIST_TOTAL AS (
+    SELECT
+        UPPER(TRIM(INFO.G_ARTIST)) AS ARTIST, 
+        COUNT(1) AS TOTAL_PLAYS
+    FROM 
+        LISTEN_HISTORY LH
+        INNER JOIN MUSIC_INFO_GOOGLE_ONLY INFO ON
+            INFO.SONG_URL = LH.SONG_URL
+    GROUP BY
+        ARTIST
 )
 
 -- Select the artist, title, and count of listens from the RANKEDSONGS CTE
 SELECT
-    ARTIST,
-    TITLE,
-    NUM
+    RS.ARTIST,
+    RS.TITLE,
+    RS.NUM,
+    TA.TOTAL_PLAYS
 FROM
-    RANKEDSONGS
+    RANKEDSONGS RS
+        LEFT OUTER JOIN ARTIST_TOTAL TA ON 
+            TA.ARTIST = RS.ARTIST
 WHERE
-    RN = 1                               -- Filter only rows with row number = 1 (top song for each artist)
-
+    RS.RN = 1                               -- Filter only rows with row number = 1 (top song for each artist)
+    AND RS.NUM >= 50
 -- Order the final results by the count of listens in descending order
 ORDER BY 
-    NUM DESC;
+    RS.NUM DESC;
