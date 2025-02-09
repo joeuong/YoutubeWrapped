d9509fd0ee2e97df1b0fa3f86f4e920f69369ba1
Joseph Uong <josephuong@Aletzia.local>
2025-02-08 19:23:30 -0500

    minor definition updates

--- update_python/_music_quarterly_run.py
+++ update_python/_music_quarterly_run.py
@@ -3,10 +3,9 @@ import listen_add
 import add_to_db_google_only 
 
 use_dir = os.getcwd()
-path = os.path.join(use_dir, '/Users/josephuong/Library/CloudStorage/Dropbox/Home-VSCode/Apps/YouTube_Wrapped/json_data/2024/2024-08-08_watch-history.json') 
-beg_date = '2024-08'
-end_date = '2024-09'
-# already updated for next time
+path = os.path.join(use_dir, '/Users/josephuong/Library/CloudStorage/Dropbox/Home-VSCode/Apps/YouTube_Wrapped/json_data/watch_history/2025-02-08_watch-history.json') 
+beg_date = '2024-12'
+end_date = '2025-01'
 
 listen_add.listen(path,beg_date,end_date)
 add_to_db_google_only.itunesAPI(path,beg_date,end_date)
